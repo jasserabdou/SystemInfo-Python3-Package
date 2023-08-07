16cbef01b3a30beb23e44036d6c28900c1afd15f
Jasser <jasserabdelfattah@12gmail.com>
2023-08-07 08:00:10 +0300

    fixed some bugs

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='systeminfo-python3',
-    version='0.1',
+    version='0.1.0',
     author='Jasser Abdelfattah',
     author_email='jasserabdelfattah12@gmail.com',
     description='A package that provides system information for your device.',
